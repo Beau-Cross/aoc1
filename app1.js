c2d0e6b9a429742d03ea0618d777e8a02c4281f9
beauho <34150946+beauho@users.noreply.github.com>
2017-12-01 03:54:25 -0600

    big finish

--- app1.js
+++ app1.js
@@ -1,3 +1,55 @@
-(function main(){
-  console.log("hello world");
-})();
+var fs = require('fs');
+//setting up some values
+var i=0;
+var s = "";
+var sum = 0;
+
+fs.readFile('input.txt','utf8', function(err, data){
+    for (i=0;i<data.length;i++) {
+
+      //fetching data
+      var dataLocationFirst = data.charAt(i);
+      var dataLocationNext = data.charAt(i+1);
+
+      //for the logs
+      s += dataLocationFirst + " ";
+
+      //conversion to numbers
+      var numberDataLocationFirst = parseInt(dataLocationFirst);
+
+      //collection of alike
+      var alike = numberDataLocationFirst;
+
+
+      //check conditions
+      if (dataLocationFirst == dataLocationNext) {
+
+        //writing the raw data
+        fs.appendFile('outputRaw.txt',alike,function(err){
+          if (err) throw err;
+        });
+
+        //summation equation
+
+      }
+    }
+
+
+    //log position
+    console.log(s);
+
+});
+
+setTimeout(function () {
+  fs.readFile('outputRaw.txt','utf8',function(err,dataRaw){
+    var sum = 0;
+    for (i=0;i<dataRaw.length;i++) {
+      var summingNumber = dataRaw.charAt(i);
+      var summingData = parseInt(summingNumber);
+      sum += summingData;
+    }
+    fs.appendFile('outputClean.txt',sum,function(err){
+      if (err) throw err;
+    });
+  });
+}, 2000);
